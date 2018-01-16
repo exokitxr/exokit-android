5266a64657c7eaabc85fc65bce8024cd0ffbf6f9
Adrian Biedrzycki <a@modules.io>
2018-01-16 05:51:18 -0800

    Update node assets

--- app/src/main/assets/node/html5.js
+++ app/src/main/assets/node/html5.js
@@ -1,1 +1,18 @@
-console.log('JNI boot html5.js 1');
+global.onSurfaceCreated = () => {
+  console.log('js onSurfaceCreated');
+};
+global.onSurfaceChanged = (width, height) => {
+  console.log('js onSurfaceChanged', {width, height});
+  gl.viewport(0, 0, width, height);
+};
+
+global.onNewFrame = (headViewMatrixFloat32Array, headQuaternionFloat32Array) => {
+  // console.log('js onNewFrame', headViewMatrixFloat32Array, headQuaternionFloat32Array);
+};
+global.onDrawEye = (eyeViewMatrixFloat32Array, eyePerspectiveMatrixFloat32Array) => {
+  // console.log('js onDrawEye', eyeViewMatrixFloat32Array, eyePerspectiveMatrixFloat32Array);
+};
+
+global.onDrawFrame = (viewMatrixFloat32Array, projectionMatrixFloat32Array) => {
+  // console.log('js onDrawFrame', viewMatrixFloat32Array, projectionMatrixFloat32Array);
+};
